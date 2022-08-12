77a31b291d04ebba34221540096dce72d4e555fc
vivekanandagajula <vivekanandagajula@gmail.com>
2022-08-12 16:05:43 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyCalc;
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 cal = new Class1();
+            Console.WriteLine($"sum = {cal.Add(5, 5)}");
+            Console.WriteLine($"Multiply = {cal.Multiply(5, 5)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyCalc/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyCalc
+{
+    public class Class1
+    { 
+            public int Add(int x, int y)
+            {
+                return x + y;
+            }
+
+            public int Sub(int x, int y)
+            {
+                return x - y;
+            }
+
+            public int Multiply(int x, int y)
+            {
+                return x * y;
+            }
+
+        }
+    }
+}
